d7ea974056e16b97299ff0e1ebdd864623283b4e
German Cruz <cruzgerman216@yahoo.com>
2020-05-11 18:51:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-      "Welcome to your app!!!! I BUILT THIdS!aa"
+      "Welcome to your app!!!! I no THIdS!aa"
   end
 
 end
